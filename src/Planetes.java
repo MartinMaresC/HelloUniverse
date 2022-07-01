8196be84595761e61563616d8769d7b9d25b61b7
Martin <martinmarescr@gmail.com>
2022-07-01 17:31:30 -0500

    Nombre total de planete dans le systema solaire

---
+++ src/Planetes.java
@@ -0,0 +1,8 @@
+public class Planetes {
+    public static void main(String[] args) {
+        int nombre = 8;
+        System.out.println("Aux dernier nouvelles; le nombre total de planetes dans le systeme solaire est de: " + nombre);
+        nombre = 9;
+        System.out.println("Il y a quelques annees cepedant, elles etaies au nombre de: " + nombre);
+    }
+}
