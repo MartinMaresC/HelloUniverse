9969005a3f91ac6b856e2fb406f0f95271d1acb4
Martin <martinmarescr@gmail.com>
2022-07-01 20:26:29 -0500

    Chaque phrase est liée a une variable

--- src/Planetes.java
+++ src/Planetes.java
@@ -1,8 +1,11 @@
 public class Planetes {
     public static void main(String[] args) {
         int nombre = 8;
-        System.out.println("Aux dernier nouvelles; le nombre total de planetes dans le systeme solaire est de: " + nombre);
+        String Phrase1 = "Aux dernier nouvelles; le nombre total de planetes dans le systeme solaire est de: " + nombre;
         nombre = 9;
-        System.out.println("Il y a quelques annees cepedant, elles etaies au nombre de: " + nombre);
+        String Phrase2 = "Il y a quelques annees cepedant, elles etaies au nombre de: " + nombre;
+
+        System.out.println(Phrase1);
+        System.out.println(Phrase2);
     }
 }
