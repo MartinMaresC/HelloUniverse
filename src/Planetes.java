a5bdf9f8cd831da17aed7a5489c04a02c46d3ee4
Martin <martinmarescr@gmail.com>
2022-07-07 00:38:25 -0500

    Chaque phrase est liée a une variable qui est incrementee par un grace a loperateur dincrementation

--- src/Planetes.java
+++ src/Planetes.java
@@ -2,7 +2,7 @@ public class Planetes {
     public static void main(String[] args) {
         int nombre = 8;
         String Phrase1 = "Aux dernier nouvelles; le nombre total de planetes dans le systeme solaire est de: " + nombre;
-        nombre = 9;
+        nombre++;
         String Phrase2 = "Il y a quelques annees cepedant, elles etaies au nombre de: " + nombre;
 
         System.out.println(Phrase1);
