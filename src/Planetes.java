41560e15a18f4dd6318e91736b66b53e22f6dcd1
Martin <martinmarescr@gmail.com>
2022-11-07 13:11:20 -0600

    If condition added

--- src/Planetes.java
+++ src/Planetes.java
@@ -1,11 +1,15 @@
 public class Planetes {
     public static void main(String[] args) {
-        int nombre = 8;
-        String Phrase1 = "Aux dernier nouvelles; le nombre total de planetes dans le systeme solaire est de: " + nombre;
-        nombre++;
-        String Phrase2 = "Il y a quelques annees cepedant, elles etaies au nombre de: " + nombre;
-
-        System.out.println(Phrase1);
-        System.out.println(Phrase2);
+        int nbPlanetes=8;
+        String phrase="les planètes du système solaire étaient au nombre de";
+        if( nbPlanetes == 8)
+        {
+            System.out.println(phrase+" "+nbPlanetes);
+        }
+        nbPlanetes=9;
+        if( nbPlanetes == 9)
+        {
+            System.out.println(phrase+" "+nbPlanetes);
+        }
     }
 }
