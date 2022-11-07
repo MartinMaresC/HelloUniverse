d810df660209e111fdbd33feaadddd5b8f67559f
Martin <martinmarescr@gmail.com>
2022-11-07 13:16:04 -0600

    If condition added

--- src/Planetes.java
+++ src/Planetes.java
@@ -1,14 +1,16 @@
 public class Planetes {
     public static void main(String[] args) {
-        int nbPlanetes=8;
+        int nbPlanetes;
+        short anee = 2003;
         String phrase="les planètes du système solaire étaient au nombre de";
-        if( nbPlanetes == 8)
+        if( anee < 2006 )
         {
+            nbPlanetes=8;
             System.out.println(phrase+" "+nbPlanetes);
         }
-        nbPlanetes=9;
-        if( nbPlanetes == 9)
+        if( anee >= 2006)
         {
+            nbPlanetes=9;
             System.out.println(phrase+" "+nbPlanetes);
         }
     }
